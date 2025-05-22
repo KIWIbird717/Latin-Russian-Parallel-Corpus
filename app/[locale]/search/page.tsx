cf2ebe5daec8ed1dfa3068e1c3fe2e341c0640da
Evgeniy Trubnikov <parmesanshees@mail.ru>
2025-05-23 02:15:11 +0300

    add Disclosure component

--- app/[locale]/search/page.tsx
+++ app/[locale]/search/page.tsx
@@ -1,87 +1,15 @@
-"use client";
-
 import { BreadcrumbHeader } from "@/shared/ui/BreadcrumbHeader";
-import { Checkbox } from "@/shared/ui/Chekbox";
-import { Input } from "@/shared/ui/Input";
-import { MultipleSelect } from "@/shared/ui/intent/ui/multiple-select";
-import { Select } from "@/shared/ui/intent/ui/select";
-import { Slider } from "@/shared/ui/intent/ui/slider";
+import { Disclosure } from "@/shared/ui/Disclosure";
 import { Page } from "@/shared/ui/Page";
-import { Search } from "@/shared/ui/Search";
-import { Tabs } from "@/shared/ui/Tabs";
-import { useState } from "react";
-
-const fruits = [
-  { id: 1, name: "Apple" },
-  { id: 2, name: "Banana" },
-  { id: 3, name: "Cherry" },
-  { id: 4, name: "Date" },
-  { id: 5, name: "Elderberry" },
-  { id: 6, name: "Fig" },
-  { id: 7, name: "Grape" },
-  { id: 8, name: "Honeydew" },
-  { id: 9, name: "Kiwi" },
-  { id: 10, name: "Lemon" },
-  { id: 11, name: "Mango" },
-  { id: 12, name: "Nectarine" },
-  { id: 13, name: "Orange" },
-  { id: 14, name: "Papaya" },
-  { id: 15, name: "Quince" },
-  { id: 16, name: "Raspberry" },
-  { id: 17, name: "Strawberry" },
-  { id: 18, name: "Tangerine" },
-  { id: 19, name: "Ugli Fruit" },
-  { id: 20, name: "Watermelon" },
-];
-
-export const software = [
-  { id: 1, name: "Adobe Photoshop" },
-  { id: 2, name: "Sketch" },
-  { id: 3, name: "Figma" },
-  { id: 4, name: "Adobe XD" },
-  { id: 5, name: "InVision" },
-];
 
 export default function SearchPage() {
-  const [isCheckbox, setIsCheckbox] = useState(false);
-
   return (
     <>
       <BreadcrumbHeader title="Поиск" />
       <Page>
-        <Tabs
-          className="w-[300px]"
-          id="huh"
-          tabs={[
-            { id: 1, label: "Huh 1" },
-            { id: 2, label: "Huh 2" },
-          ]}
-        />
-        <Input label="Label" />
-        <Search label="Search" />
-
-        <MultipleSelect className="max-w-xs" label="Fruits" items={fruits}>
-          {(item) => {
-            return <MultipleSelect.Item textValue={item.name}>{item.name}</MultipleSelect.Item>;
-          }}
-        </MultipleSelect>
-
-        <Select label="Design software" placeholder="Select a software" className="max-w-xs">
-          <Select.Trigger />
-          <Select.List items={software}>
-            {(item) => (
-              <Select.Option id={item.id} textValue={item.name}>
-                {item.name}
-              </Select.Option>
-            )}
-          </Select.List>
-        </Select>
-
-        <Checkbox isSelected={isCheckbox} onChange={setIsCheckbox}>
-          Checkout
-        </Checkbox>
-
-        <Slider label="Slider" className="max-w-xs" postfix="Postfix" />
+        <Disclosure label="Авторы и Оперы" className="max-w-[500px]">
+          <div className="h-[100px] w-[100px] bg-red-100" />
+        </Disclosure>
       </Page>
     </>
   );
---
+++ shared/ui/Disclosure.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Typography } from "@/shared/ui/Typography";
+import { AnimatePresence, motion } from "motion/react";
+import { ComponentProps, FC, ReactNode, useState } from "react";
+import ChevronSvg from "@/public/svg/chevron.svg";
+import { cn } from "@/shared/lib/utils";
+
+type DisclosureProps = ComponentProps<"div"> & { label: ReactNode };
+export const Disclosure: FC<DisclosureProps> = ({ children, label, className, ...props }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div {...props} className={cn("gap-md border-border-stroke flex flex-col border-b", className)}>
+      <button
+        onClick={() => setIsOpen((state) => !state)}
+        className="gap-lg flex cursor-pointer items-center justify-between"
+      >
+        <Typography.Heading4>{label}</Typography.Heading4>
+        <ChevronSvg className={cn("transition duration-100", isOpen && "-rotate-180")} />
+      </button>
+
+      <AnimatePresence initial={false}>
+        <motion.div
+          className="gap-md flex flex-col overflow-hidden"
+          animate={{ height: isOpen ? "inherit" : 0 }}
+        >
+          {children}
+          <div className="h-[8px] w-full" />
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+};
